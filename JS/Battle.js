75bf4d289187d00a852e17c02cede06484289b4b
Liang <taylor721@gmail.com>
2020-09-05 15:50:27 -0600

    add winnercheck cathch

--- JS/Battle.js
+++ JS/Battle.js
@@ -60,11 +60,16 @@ let userHealthPointDisplay = document.getElementById("PH-area-user-health-point-
 let userHealthPointMax = document.getElementById('PH-area-user-health-point-max');
 let userSpiritPiontDisplay = document.getElementById('spirit-bar-max-current');
 let userSkillBtnName = document.getElementById('skill-btn');
+let userPHDamageDisplay = document.getElementById('user-PH-Damage');
+let userCatchBtnDisplay = document.getElementById('battle-area-user-character-btn');
+let userAttriChangeDisplay = document.getElementById('user-attribute-change');
+let userRoundInfo = document.getElementById('user-round-info');
+let userTextInfo = document.getElementById('move-status-user');
 
 /* set com variable */
 let comName = kubfu.name;
 let comPHmax = kubfu.PHmax;
-let comPHcurrent = comPHmax; //need cookie
+let comPHcurrent = 100; //need cookie
 let comAttackMax = kubfu.attackMax; //need cookie
 let comAttackMin = kubfu.attackMin; //need cookie
 let comDefense = kubfu.defense; //need cookie
@@ -74,12 +79,16 @@ let comAttack;
 let comDamage;
 
 let comNameDisplay = document.getElementById('PH-area-com-name');
+let userPokemonImg = document.getElementById('battle-area-user-pokemon-img');
 let comHealthPrecent = comPHcurrent / comPHmax;
 let comHealthBarDisplay = document.getElementById('PH-area-com-health-bar-content');
 let comHealthPointDisplay = document.getElementById("PH-area-com-health-point-left");
 let comHealthPointMax = document.getElementById('PH-area-com-health-point-max');
-let statusDisplay2 = document.getElementById('move-status-com-text2');
-let userPHDamageDisplay = document.getElementById('user-PH-Damage');
+let comPHDamageDisplay = document.getElementById('com-PH-Damage');
+let comAttriChangeDisplay = document.getElementById('com-attribute-change');
+let comRoundInfo = document.getElementById('com-round-info');
+let comTextInfo = document.getElementById('move-status-com');
+
 
 /*declare other variable */
 let userTimerNum = 0; //need cookie
@@ -88,28 +97,37 @@ let userSpiritBar = document.getElementById('timer-user-bar-current');
 let comSpiritBar = document.getElementById('timer-com-bar-current');
 let userMoveBtn = document.getElementById('battle-area-user-btn');
 let userArea = document.getElementById('battle-area-user');
+let timerDisplay = document.getElementById('timer');
 let medicine = 50;
 
 
 
 /*set background status */
 document.body.style.backgroundImage = "URL('../images/battle1Backgroud.jpg')"; //How to use a variable in right quote?
+userPokemonImg.src = userImgPath;
 
 userNameDisplay.innerHTML = userName.toUpperCase(); //user name display
 UserHealthDisplay(); //initial user health display
 
 comNameDisplay.innerHTML = comName.toUpperCase(); //com name display
 ComHealthDisplay(); //initial com health display
-// userMoveBtn.style.display = "none"; // do not display the move button 
+userMoveBtn.style.display = "none"; // do not display the move button 
 // userSkillBtnName.style.display = "none"; // do not display the skill button
 UserSpiritCheck(); //initial user spirit display
-statusDisplay2.style.display = "none";
 userPHDamageDisplay.style.display = "none";
-console.log(userAttackMax);
-console.log(userAttackMin);
+userAttriChangeDisplay.style.display = "none";
+userRoundInfo.style.display = "none";
+comPHDamageDisplay.style.display = "none";
+comAttriChangeDisplay.style.display = "none";
+comRoundInfo.style.display = "none";
+
+
 /* status functions start*/
 /* user health  display*/
 function UserHealthDisplay() {
+    if (userPHcurrent <= 0) {
+        userPHcurrent = 0;
+    }
     userHealthPrecent = userPHcurrent / userPHmax;
     userHealthBarDisplay.style.width = userHealthPrecent * 60 + 'vw';
     if (userHealthPrecent >= 0.8) {
@@ -127,8 +145,12 @@ function UserHealthDisplay() {
     userHealthPointMax.innerText = userPHmax;
 }
 
+
 /* com health display*/
 function ComHealthDisplay() {
+    if (comPHcurrent <= 0) {
+        comPHcurrent = 0;
+    }
     comHealthPrecent = comPHcurrent / comPHmax;
     comHealthBarDisplay.style.width = comHealthPrecent * 60 + 'vw';
     if (comHealthPrecent >= 0.8) {
@@ -144,15 +166,18 @@ function ComHealthDisplay() {
     }
     comHealthPointDisplay.innerText = comPHcurrent;
     comHealthPointMax.innerText = comPHmax;
+    CatchCheck();
 }
 
 /* timer display */
 function TimerStart() {
     userMoveBtn.style.display = "none";
+    document.getElementById("start-battle").style.display = "none";
     userArea.style.transform = 'scale(1.2)';
-    userArea.style.left = "10vw";
+    userArea.style.left = "16vw";
     let userTimer = setInterval(UserTimer, userSpeed);
     let comTimer = setInterval(ComTimer, comSpeed);
+    console.log(userTimerNum);
 
     function UserTimer() {
         userTimerNum++;
@@ -193,7 +218,7 @@ function TimerStart() {
 
 /*spirit gathering */
 function UserSpiritGatheringBar() {
-    userSpiritPoint = (Math.floor(Math.random() * (30 - 10)) + 10) + userSpiritPoint;
+    userSpiritPoint = (Math.floor(Math.random() * (50 - 20)) + 20) + userSpiritPoint;
     console.log(userSpiritPoint);
     if (userSpiritPoint >= 100) {
         userSpiritPoint = 100;
@@ -203,8 +228,10 @@ function UserSpiritGatheringBar() {
 
 function UserSpiritCheck() {
     if (userSpiritPoint >= 100) {
-        userSkillBtnName.style.display = "block";
-        userSkillBtnName.innerHTML = userSkillName + " / T";
+        setTimeout(() => {
+            userSkillBtnName.style.display = "block";
+        }, 2000);
+        userSkillBtnName.innerHTML = userSkillName + "<br> T";
         userSpiritPiontDisplay.style.width = userSpiritPoint / 2 + "vw";
     } else {
         userSpiritPiontDisplay.style.width = userSpiritPoint / 2 + "vw";
@@ -229,19 +256,19 @@ function UserSpiritCheck() {
 function StopRoundCheck() {
     if (userRestStopRound < 0) {
         userRestStopRound = -1;
-        statusDisplay2.style.display = 'none';
+        comRoundInfo.style.display = 'none';
         comSpeed = kubfu.speed;
     } else if (userRestStopRound === 0) {
-        statusDisplay2.style.display = 'block';
-        statusDisplay2.innerHTML = 'Stop round left : ' + userRestStopRound;
+        comRoundInfo.style.display = 'block';
+        comRoundInfo.innerHTML = 'Stop round left : ' + userRestStopRound;
         userRestStopRound--;
         comSpeed = kubfu.speed;
         setTimeout(() => {
-            statusDisplay2.style.display = 'none';
+            comRoundInfo.style.display = 'none';
         }, 2000);
     } else {
-        statusDisplay2.style.display = 'block';
-        statusDisplay2.innerHTML = 'Stop round left : ' + userRestStopRound;
+        comRoundInfo.style.display = 'block';
+        comRoundInfo.innerHTML = 'Stop round left : ' + userRestStopRound;
         userRestStopRound--;
     }
 }
@@ -255,6 +282,36 @@ function BuffRoundCheck() {
         comDefense = comDefense + userDebuff;
     } else {
         userRestBuffRound--;
+
+    }
+}
+/*Catch check */
+function CatchCheck() {
+
+    if (comPHcurrent / comPHmax < 0.2) {
+        userCatchBtnDisplay.style.display = "block";
+        console.log(userCatchBtnDisplay);
+    }
+}
+
+/*winner check */
+function WinnerCheck() {
+    if (userPHcurrent > 0 && comPHcurrent <= 0) {
+        document.getElementById('battle').remove()
+        document.getElementById('timer').remove();
+        var result = document.createElement("section");
+        result.innerHTML = '<p><span id="winner"></span> WIN!!</p><img id = "winner image" src="">';
+        document.body.append(result);
+        document.getElementById('winner').innerText = userName;
+        document.getElementById("winner image").src = userImgPath;
+    } else if (comPHcurrent > 0 && userPHcurrent <= 0) {
+        document.getElementById('battle').remove()
+        document.getElementById('timer').remove();
+        var result = document.createElement("section");
+        result.innerHTML = '<p><span id="winner"></span> WIN!!</p><img id = "winner image" src="">';
+        document.body.append(result);
+        document.getElementById('winner').innerText = comName;
+        document.getElementById("winner image").src = comImgPath;
     }
 }
 /* status functions end*/
@@ -277,16 +334,35 @@ function UserAttack() { // user noraml attack
     userAttack = Math.floor(Math.random() * (userAttackMax - userAttackMin)) + userAttackMin;
     comDamage = userAttack - comDefense;
     comPHcurrent = comPHcurrent - comDamage;
+
+    comPHDamageDisplay.style.display = "block";
+    comTextInfo.style.animation = "TextDisplay 0.6s linear";
+    comPHDamageDisplay.innerHTML = "- PH " + comDamage;
+    setTimeout(() => {
+        comPHDamageDisplay.style.display = "none";
+    }, 500);
+
     ComHealthDisplay();
     UserSpiritGatheringBar();
+    WinnerCheck();
     setTimeout(TimerStart, 2000);
+
 }
 
 function ComAttack() { // com normal  attack
     comAttack = Math.floor(Math.random() * (comAttackMax - comAttackMin)) + comAttackMin;
     userDamage = comAttack - userDefense;
     userPHcurrent = userPHcurrent - userDamage;
+
+    userPHDamageDisplay.style.display = "block";
+    userTextInfo.style.animation = "TextDisplay 0.6s linear";
+    userPHDamageDisplay.innerHTML = "- PH " + userDamage;
+    setTimeout(() => {
+        userPHDamageDisplay.style.display = "none";
+    }, 500);
+
     UserHealthDisplay();
+    WinnerCheck();
     setTimeout(TimerStart, 2000);
 }
 /* user skill attack */
@@ -305,11 +381,19 @@ function UserSkillAttack() {
         userSkillAttack = userAttack;
     }
     comDamage = userSkillAttack - comDefense;
+    comPHcurrent = comPHcurrent - comDamage;
+    comPHDamageDisplay.style.display = "block";
+    comTextInfo.style.animation = "TextDisplay 0.6s linear";
+    comPHDamageDisplay.innerHTML = "- PH " + comDamage;
+    setTimeout(() => {
+        comPHDamageDisplay.style.display = "none";
+    }, 500);
 
     userSpiritPoint = 0;
     console.log(comDamage);
     ComHealthDisplay();
     UserSpiritCheck();
+    WinnerCheck();
     setTimeout(TimerStart, 2000);
 }
 
@@ -326,15 +410,40 @@ function UserStopAttack() {
 
 /* buff attack */
 function UserBuffAttack() {
-    userAttackMax = userAttackMax + userBuff;
-    userAttackMin = userAttackMin + userBuff;
-    comDefense = comDefense - userDebuff;
-    userRestBuffRound = 3;
-    ComHealthDisplay();
-    UserSpiritCheck();
-    setTimeout(TimerStart, 2000);
-}
+    if (userRestBuffRound >= 0) {
+        comAttriChangeDisplay.style.display = "block";
+        comAttriChangeDisplay.innerHTML = "You can not repeat the Buff";
+        setTimeout(() => {
+            comAttriChangeDisplay.style.display = "none";
+        }, 1000);
+    } else {
+        userAttackMax = userAttackMax + userBuff;
+        userAttackMin = userAttackMin + userBuff;
+        comDefense = comDefense - userDebuff;
+        if (comDefense <= 0) {
+            comDefense = 0;
+        }
 
+        comAttriChangeDisplay.style.display = "block";
+        comTextInfo.style.animation = "TextDisplay 0.6s linear";
+        comAttriChangeDisplay.innerHTML = "- Defense" + userDebuff;
+        setTimeout(() => {
+            comAttriChangeDisplay.style.display = "none";
+        }, 500);
+
+        userAttriChangeDisplay.style.display = "block";
+        userTextInfo.style.animation = "TextDisplay 0.6s linear";
+        userAttriChangeDisplay.innerHTML = "+ Attack" + userBuff;
+        setTimeout(() => {
+            userAttriChangeDisplay.style.display = "none";
+        }, 500);
+
+        userRestBuffRound = 3;
+        ComHealthDisplay();
+        UserSpiritCheck();
+        setTimeout(TimerStart, 2000);
+    }
+}
 /*healing */
 function UserHealing() {
     StopRoundCheck();
@@ -343,12 +452,29 @@ function UserHealing() {
     if (userPHcurrent >= userPHmax) {
         userPHcurrent = userPHmax;
     }
+    userTextInfo.style.animation = "TextDisplay 0.6s linear";
     userPHDamageDisplay.style.display = "block";
     userPHDamageDisplay.innerHTML = "+ PH" + medicine;
     setTimeout(() => {
         userPHDamageDisplay.style.display = "none";
-    }, 2000);
+    }, 500);
     UserHealthDisplay();
     UserSpiritCheck();
     setTimeout(TimerStart, 2000);
+}
+
+/* Catch Pokemon */
+function CatchPokemon() {
+    document.getElementById('battle').remove()
+    document.getElementById('timer').remove();
+    var result = document.createElement("section");
+    document.body.append(result);
+    result.innerHTML = '<img id = "winner image" src="">';
+    document.getElementById("winner image").src = "../images/pokeBall.png";
+    setTimeout(() => {
+        result.innerHTML = '<p>You caught<span id="winner"></span></p><img id = "winner image" src="">';
+        document.getElementById('winner').innerText = comName;
+        document.getElementById("winner image").src = comImgPath;
+    }, 3000);
+
 }
